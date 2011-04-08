182d2be13994d516fc593446fe6cc95bab3fe2f6
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2011-04-08 21:04:43 +0300

    still can't render to bitmap...

--- Rotating Textures/main.cpp
+++ Rotating Textures/main.cpp
@@ -61,15 +61,19 @@ int main()
 	return message.wParam;
 } // end function main
 
+void display();
+float angle = 0; // in degrees
 
 LRESULT CALLBACK soleWindowProcedure(HWND window,UINT message,WPARAM argW,LPARAM argL)
 {
 	static PIXELFORMATDESCRIPTOR pixelFormatDescription = {0};
 	static HDC windowSurface;
 	static HGLRC glContext;
-	static float angle = 0; // in degrees
 	int pixelFormatIndex;
 	RECT r;
+	HBITMAP gdiBitmap;
+	static HDC gdiMemContext;
+	static HGLRC glMemContext;
 
 	switch(message)
 	{
@@ -94,39 +98,37 @@ LRESULT CALLBACK soleWindowProcedure(HWND window,UINT message,WPARAM argW,LPARAM
 		glewInit();
 
 		glClearColor(1,1,0.941,1);	// ivory
+
+		ZeroMemory(&pixelFormatDescription,sizeof(PIXELFORMATDESCRIPTOR));
+		pixelFormatDescription.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+		pixelFormatDescription.nVersion = 1;
+		pixelFormatDescription.dwFlags = PFD_DRAW_TO_BITMAP|PFD_SUPPORT_OPENGL|PFD_STEREO_DONTCARE;
+		pixelFormatDescription.iPixelType = PFD_TYPE_RGBA;
+		pixelFormatDescription.cColorBits = 64;
+		pixelFormatDescription.cAlphaBits = 16;
+		pixelFormatDescription.cAccumBits = 128;
+		pixelFormatDescription.cDepthBits = 64;
+		pixelFormatDescription.cStencilBits = 32;
+		pixelFormatDescription.cAuxBuffers = 128;
+		pixelFormatDescription.iLayerType = PFD_MAIN_PLANE;
+
+		gdiMemContext = CreateCompatibleDC(windowSurface);
+		pixelFormatIndex = ChoosePixelFormat(gdiMemContext,&pixelFormatDescription);
+		SetPixelFormat(gdiMemContext,pixelFormatIndex,&pixelFormatDescription);
+		glMemContext = wglCreateContext(gdiMemContext);
+		wglMakeCurrent(gdiMemContext,glMemContext);
+
+		glClearColor(1,1,0.941,1);	// ivory
+
+		wglMakeCurrent(windowSurface,glContext);
 		return 0;
 	case WM_SIZE:
 		GetClientRect(window,&r);
 		glViewport(0,0,r.right,r.bottom);
 		return 0;
 	case WM_PAINT:
-		glClear(GL_COLOR_BUFFER_BIT);
-			glColor3f(1,0,0);	// red
-			glLoadIdentity();
-			glTranslatef(0.5,0.5,0);
-			glRotatef(angle,0,0,1);
-			glRectf(-1.0/3,-1.0/3,1.0/3,1.0/3);
-
-			glColor3f(0,1,0);	// green
-			glLoadIdentity();
-			glTranslatef(-0.5,0.5,0);
-			glRotatef(angle,0,0,1);
-			glRectf(-1.0/3,-1.0/3,1.0/3,1.0/3);
-
-			glColor3f(0,0,1);	// blue
-			glLoadIdentity();
-			glTranslatef(-0.5,-0.5,0);
-			glRotatef(angle,0,0,1);
-			glRectf(-1.0/3,-1.0/3,1.0/3,1.0/3);
-
-			glColor3f(1,1,0);	// yellow
-			glLoadIdentity();
-			glTranslatef(0.5,-0.5,0);
-			glRotatef(angle,0,0,1);
-			glRectf(-1.0/3,-1.0/3,1.0/3,1.0/3);
-
-			glPopMatrix();
-			angle += 1.5;
+		display();
+		angle += 1.5;
 		SwapBuffers(windowSurface);
 		ValidateRect(window,nullptr);
 		InvalidateRect(window,nullptr,FALSE);
@@ -148,12 +150,31 @@ LRESULT CALLBACK soleWindowProcedure(HWND window,UINT message,WPARAM argW,LPARAM
 		case IDM_FILE_OPEN:
 			MessageBeep(0);
 			return 0;
+		case IDM_EDIT_COPY:
+			GetClientRect(window,&r);
+			gdiBitmap = CreateCompatibleBitmap(gdiMemContext,r.right,r.bottom);
+			gdiBitmap = (HBITMAP)SelectObject(gdiMemContext,gdiBitmap);
+				wglMakeCurrent(gdiMemContext,glMemContext);
+					glViewport(0,0,r.right,r.bottom);
+					display();
+					//SwapBuffers(gdiMemContext);
+				wglMakeCurrent(windowSurface,glContext);
+				//BitBlt(gdiMemContext,0,0,r.right,r.bottom,windowSurface,0,0,SRCCOPY);
+			gdiBitmap = (HBITMAP)SelectObject(gdiMemContext,gdiBitmap);
+
+			OpenClipboard(window);
+			EmptyClipboard();
+				SetClipboardData(CF_BITMAP,gdiBitmap);
+			CloseClipboard();
+			return 0;
 		} // end switch
 		return 0;
 	case WM_ERASEBKGND:
 		return 1;
 	case WM_DESTROY:
 		wglDeleteContext(glContext);
+		wglDeleteContext(glMemContext);
+		DeleteDC(gdiMemContext);
 		//ReleaseDC(window,windowSurface);
 		PostQuitMessage(0);
 		return 0;
@@ -161,3 +182,32 @@ LRESULT CALLBACK soleWindowProcedure(HWND window,UINT message,WPARAM argW,LPARAM
 	return DefWindowProc(window,message,argW,argL);
 } // end function soleWindowProcedure
 
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1,0,0);	// red
+	glLoadIdentity();
+	glTranslatef(0.5,0.5,0);
+	glRotatef(angle,0,0,1);
+	glRectf(-1.0/3,-1.0/3,1.0/3,1.0/3);
+
+	glColor3f(0,1,0);	// green
+	glLoadIdentity();
+	glTranslatef(-0.5,0.5,0);
+	glRotatef(angle,0,0,1);
+	glRectf(-1.0/3,-1.0/3,1.0/3,1.0/3);
+
+	glColor3f(0,0,1);	// blue
+	glLoadIdentity();
+	glTranslatef(-0.5,-0.5,0);
+	glRotatef(angle,0,0,1);
+	glRectf(-1.0/3,-1.0/3,1.0/3,1.0/3);
+
+	glColor3f(1,1,0);	// yellow
+	glLoadIdentity();
+	glTranslatef(0.5,-0.5,0);
+	glRotatef(angle,0,0,1);
+	glRectf(-1.0/3,-1.0/3,1.0/3,1.0/3);
+
+} // end function display
